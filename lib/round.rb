7be86b53e5c14069c80a23572dbc4f6399303142
thatvictor7 <montoya.victor7@gmail.com>
2018-01-29 09:31:40 -0700

    commit

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,5 @@
 require_relative 'guess'
 
-require 'pry'
 class Round
   attr_reader :deck, :guesses
   attr_accessor :current_card
@@ -16,15 +15,16 @@ class Round
   end
 
   def current_card
-    @deck.cards[@current_card]
+    @deck.cards[@current_index]
   end
 
   def record_guess(user_guess)
     new_guess = guess_card(user_guess, current_card)
     @guesses.push(new_guess)
     #switch to next card
-    @current_card += 1
-    return new_guess
+    @current_index += 1
+    #return new_guess
+    return @guesses
   end
 
   def insert_guess(user_guess, card)
@@ -37,7 +37,10 @@ class Round
 
   def number_correct
     #want to check every guess, if correct add count
-    @guesses.each {|guess| if guess.response == guess.card.answer}
+    @guesses.each do |guess|
+      if guess.response == guess.card.answer
+      end
+    end
   end
 
   def guess_card(user_guess, trivia_card)
